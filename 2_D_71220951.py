05e0eb72f994918f5ac4f974f952b13a63aa5826
beatricedwiputri <117900796+beatricedwiputri@users.noreply.github.com>
2022-12-01 16:50:01 +0700

    Add files via upload

---
+++ 2_D_71220951.py
@@ -0,0 +1,13 @@
+#program genap ganjil ambil 3 huruf dari kanan
+masukinkata = input("Masukkan kata :")
+def apalahiniakuberusahabangetkaksemogabisaupdatebisakak():
+    list = (masukinkata)
+    len(masukinkata)
+    if(len(masukinkata)%2==0):
+        print("Huruf paling ujung pada kata", masukinkata, "adalah", (masukinkata[-3:]))
+    else:
+        print("Huruf paling ujung pada kata", masukinkata, "adalah",(masukinkata[:3]))
+    return (masukinkata)
+apalahiniakuberusahabangetkaksemogabisaupdatebisakak()
+
+
