75f85956b9f402c7dfc6f8bad9535fd917b7a3ba
beatricedwiputri <117900796+beatricedwiputri@users.noreply.github.com>
2022-12-01 16:09:59 +0700

    Add files via upload

---
+++ 1_D_71220951.py
@@ -0,0 +1,88 @@
+print ("Halo selamat datang di bioksop!")
+print ("Dimanakah kamu ingin menonton?")
+print ("1) XXI Empire")
+print ("2) XXI Amplaz")
+print ("3) XXI JCM")
+pilihanmu = input("Masukkan pilihanmu:")
+
+def pilihannonton():
+    if(pilihanmu == "1"):
+        print("Mau nonton film apa nih? Ada film:")
+        print("1. Frozen")
+        print("2. Keramat")
+        print("3. KKN di Desa Penari")
+    elif(pilihanmu == "2"):
+        print("Mau nonton film apa nih? Ada film:")
+        print("1. Frozen")
+        print("2. Keramat")
+        print("3. KKN di Desa Penari")
+    elif(pilihanmu == "3"):
+        print("Mau nonton film apa nih? Ada film:")
+        print("1. Frozen")
+        print("2. Keramat")
+        print("3. KKN di Desa Penari")
+    else:
+        print("Pilihan tidak tersedia")
+
+pilihannonton()
+
+pilihnomorfilem = input("Pilih nomor film:")
+def pilihfilm():
+    if(pilihnomorfilem == "1"):
+        print("Mau nonton layar bisoskop apa:")
+        print("1. Reguler")
+        print("2. Dolby almos")
+        print("3. Premiere")
+    elif(pilihnomorfilem == "2"):
+        print("Mau nonton layar bisoskop apa:")
+        print("1. Reguler")
+        print("2. Dolby almos")
+        print("3. Premiere")
+    elif(pilihnomorfilem == "3"):
+        print("Mau nonton layar bisoskop apa:")
+        print("1. Reguler")
+        print("2. Dolby almos")
+        print("3. Premiere")
+    else:
+        print("Pilihan tidak tersedia")
+    
+pilihfilm()
+
+pilihtipelayar = input("Pilih nomor tipe layar:")
+def pilihlayar():
+    if(pilihtipelayar == "1"):
+        input("Berapa banyak tiket?")
+        print("Mau nonton jam berapa:")
+        print("1. 12.35")
+        print("2. 14.45")
+        print("3. 16.55")
+        print("4. 19.05")
+    elif(pilihtipelayar == "2"):
+        input("Berapa banyak tiket?")
+        print("Mau nonton jam berapa:")
+        print("1. 12.35")
+        print("2. 14.45")
+        print("3. 16.55")
+        print("4. 19.05")
+    elif(pilihtipelayar == "3"):
+        input("Berapa banyak tiket?")
+        print("Mau nonton jam berapa:")
+        print("1. 12.35")
+        print("2. 14.45")
+        print("3. 16.55")
+        print("4. 19.05")
+pilihlayar()
+
+pilihanjam = input("Masukkan angka pilihan Anda:")
+def pilihanjamhehe():
+    if(pilihanjam == "1"):
+        print("Oke berhasil, silahkan dinikmati di jam 12.35")
+    elif(pilihanjam == "2"):
+        print("Oke berhasil, silahkan dinikmati di jam 14.45")
+    elif(pilihanjam == "3"):
+        print("Oke berhasil, silahkan dinikmati di jam 16.55")
+    elif(pilihanjam == "4"):
+        print("Oke berhasil, silahkan dinikmati di jam 19.05")
+    else:
+        pass
+pilihanjamhehe()
